09e68180c5af9838f0c53b7e2487d49e40079226
Nathan <shweppsie@gmail.com>
2011-04-21 20:38:46 +1200

    add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="vc",
+        version="0.1",
+        description="Control Output of a program",
+        author="Nathan Overall",
+        author_email="shweppsie@gmail.com",
+        url="http://www.github.com/shweppsie/VerboseControl/",
+	modules=['vc'],
+)
+
