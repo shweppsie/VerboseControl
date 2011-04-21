f8fe1c4f64ba5b4a6dd3a878660cfc4405f22e13
Nathan <shweppsie@gmail.com>
2011-04-22 02:29:38 +1200

    document output levels and create a list of them

--- vc.py
+++ vc.py
@@ -1,5 +1,26 @@
 import sys
 
+"""
+Totally Silent: Output nothing. stderr will still be output
+Silent: Only output Errors -> Use this level for when the program hits an
+	urecoverable error
+Warning: Only output Warnings -> Use this level for when the program hits a
+	recoverable error
+Normal: Just output text normally -> Use this for anything you want the user to
+	see if they don't change the verbosity
+Info: Output progress information -> Use this for telling the user the progress
+	of actions
+Verbose: Output everything -> Use this level for information which doesn't
+	relate directly to the progress of the application but the user may
+	still find useful
+Debug: Output development information -> information for debugging the
+	application. Probably only useful for developers
+
+"""
+
+VERBOSITY={ -1:'Totally Silent', 0:'Silent', 1:'Warning', 2:'Normal', 3:'Info', 4:'Verbose', 5:'Debug'}
+STRINGS={ 0:'ERROR', 1:'WARNING', 3:'INFO', 4:'VERBOSE', 5:'DEBUG' }
+
 class VerboseControl:
 	def __init__(self,verbosity):
 		self.verbosity = verbosity
